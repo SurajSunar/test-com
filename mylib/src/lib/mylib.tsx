83a6da8357d51b33fc8d3383e2af78bf85e097f0
Suraj Sunar <surbhu06@gmail.com>
2026-04-09 21:17:28 +0600

    modified message

--- mylib/src/lib/mylib.tsx
+++ mylib/src/lib/mylib.tsx
@@ -1,5 +1,11 @@
+import { useEffect } from 'react';
+
 export function TestComMylib() {
   const defaultVal = 'This is test message';
+  useEffect(() => {
+    //add logic here for init
+  });
+
   return (
     <div>
       <h1>Welcome to {defaultVal}</h1>
